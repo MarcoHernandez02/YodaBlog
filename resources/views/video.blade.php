289e55d2425c1d81d70420e63689e86b14abde06
MarcoHernandez02 <marco_1320114125@uptecamac.edu.mx>
2022-04-02 11:39:58 -0600

    Video update

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -1,5 +1,9 @@
 @extends('Template.layout')
     @section('seccion2')
-       
-    @endsection
+       <h2>CONTROLLER VIDEO</h2>
+       <iframe width="560" height="315" src="https://www.youtube.com/embed/KkpkimMmCQI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    <br>
+    <<h2>API VIDEO</h2>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/RvZ82l8jXco" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        @endsection
 
